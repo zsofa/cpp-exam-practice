f4de7ef07b269c8725254a244f6c03a0f12238b6
zsofa <hzsofi12@gmai.com>
2024-06-09 19:29:27 +0200

    add helloword

---
+++ helloword.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main() {
+    std::cout << "************\n";
+    std::cout << "This repositpory is made for the exam tasks in C++\n";
+    std::cout << "************\n";
+    return 0;
+}
